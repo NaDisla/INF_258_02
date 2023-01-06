140b2c31df9479655673a85d5b34ca9035516b23
NaDisla <tejadanadiam@gmail.com>
2023-01-06 18:49:49 -0400

    Init repo

---
+++ main.py
@@ -0,0 +1,9 @@
+nombre = "Reynel" # Tipo de dato string
+num1 = 10 # Tipo de dato entero
+num2 = 4.5 # Tipo de dato float
+is_student = False # Tipo de dato boolean
+is_student = True # Tipo de dato boolean
+"""
+Comentario multilínea
+"""
+
