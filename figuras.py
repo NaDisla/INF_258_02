b5ba3326d7a5c1c321e858a4dbeefd2f05d0f641
NaDisla <tejadanadiam@gmail.com>
2023-02-17 18:35:14 -0400

    Clase de Librerías/Módulos

---
+++ figuras.py
@@ -0,0 +1,9 @@
+from math import pi
+def area_circulo(r):
+    return pi * r**2
+
+def area_cuadrado(l):
+    return l**2
+
+def area_triangulo(b, h):
+    return (b * h) / 2
---
+++ lesson08.py
@@ -0,0 +1,17 @@
+# LIBRERÍAS O MÓDULOS
+import math # IMPORTANTO LIBRERÍA
+from math import sqrt, sin, cos, tan # IMPORTANDO ELEMENTOS DE LA LIBRERÍA
+import numpy as mi_libreria # IMPORTANDO LIBRERÍA CON UN ALIAS
+import figuras # LIBRERÍA PERSONAL 
+
+area_circulo = figuras.area_circulo(15)
+area_triangulo = figuras.area_triangulo(10, 5)
+area_cuadrado = figuras.area_cuadrado(5)
+print(area_circulo)
+print(area_triangulo)
+print(area_cuadrado)
+
+raiz = sqrt(81)
+sen = sin(45)
+print(raiz)
+print(sen)
